70ec617a93b7e0ac5b4c5fe8579267ac9aeb61ca
therealh4ri <97845507+therealh4ri@users.noreply.github.com>
2024-02-28 21:41:09 +0530

    Time: 39 ms (39.81%), Space: 16.5 MB (99.65%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,15 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-        s_ptr = 0
-        for char in t:
-            if s[s_ptr]==char:
-                s_ptr+=1
-                if s_ptr==len(s):
-                    return True
-        return False
-            
+        t=iter(t)
+        return all(char in t for char in s)
                 
                 
         
